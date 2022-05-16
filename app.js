253cbb10ebed7e642f907c57cd03953e885e2487
Dilruba Tunçez <dirruba_2000@hotmail.com>
2022-05-17 01:10:06 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -37,15 +37,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 let totalFour = squares[i+3].innerHTML
                 let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
 
-                console.log(row)
-
                 let filteredRow = row.filter(num => num)
-                console.log(filteredRow)
                 let missing = 4 - filteredRow.length
                 let zeros = Array(missing).fill(0)
-                console.log(zeros)
                 let newRow = zeros.concat(filteredRow)
-                console.log(newRow)
 
                 squares[i].innerHTML = newRow[0]
                 squares[i+1].innerHTML = newRow[1]
@@ -54,13 +49,131 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     }
-    moveRight()
+
+    //swipe left
+    function moveLeft() {
+        for (let i = 0; i < 16; i++) {
+            if (i % 4 === 0) {
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i+1].innerHTML
+                let totalThree = squares[i+2].innerHTML
+                let totalFour = squares[i+3].innerHTML
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+                let filteredRow = row.filter(num => num)
+                let missing = 4 - filteredRow.length
+                let zeros = Array(missing).fill(0)
+                let newRow = filteredRow.concat(zeros)
+
+                squares[i].innerHTML = newRow[0]
+                squares[i+1].innerHTML = newRow[1]
+                squares[i+2].innerHTML = newRow[2]
+                squares[i+3].innerHTML = newRow[3]
+            }
+        }
+    }
+
+    //swipe down
+    function moveDown() {
+        for (let i = 0; i < 4; i++) {
+            let totalOne = squares[i].innerHTML
+            let totalTwo = squares[i+width].innerHTML
+            let totalThree = squares[i+(width*2)].innerHTML
+            let totalFour = squares[i+(width*3)].innerHTML
+            let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+            let filteredColumn = column.filter(num => num)
+            let missing = 4 - filteredColumn.length
+            let zeros = Array(missing).fill(0)
+            let newColumn = zeros.concat(filteredColumn)
+
+            squares[i].innerHTML = newColumn[0]
+            squares[i+width].innerHTML = newColumn[1]
+            squares[i+(width*2)].innerHTML = newColumn[2]
+            squares[i+(width*3)].innerHTML = newColumn[3]
+        }
+    }
 
 
+    //swipe up
+    function moveUp() {
+        for (let i = 0; i < 4; i++) {
+            let totalOne = squares[i].innerHTML
+            let totalTwo = squares[i+width].innerHTML
+            let totalThree = squares[i+(width*2)].innerHTML
+            let totalFour = squares[i+(width*3)].innerHTML
+            let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
 
+            let filteredColumn = column.filter(num => num)
+            let missing = 4 - filteredColumn.length
+            let zeros = Array(missing).fill(0)
+            let newColumn = filteredColumn.concat(zeros)
 
+            squares[i].innerHTML = newColumn[0]
+            squares[i+width].innerHTML = newColumn[1]
+            squares[i+(width*2)].innerHTML = newColumn[2]
+            squares[i+(width*3)].innerHTML = newColumn[3]
+        }
+    }
 
+    function combineRow() {
+        for (let i = 0; i < 15; i++) {
+            if (squares[i].innerHTML === squares[i+1].innerHTML) {
+                let combinedTotal = parseInt(squares[i].innerHTML) * parseInt(squares[i+1].innerHTML)
+                squares[i].innerHTML = combinedTotal
+                squares[i+1].innerHTML = 0
+            }
+        }
+    }
 
+    
+    function combineColumn() {
+        for (let i = 0; i < 12; i++) {
+            if (squares[i].innerHTML === squares[i+width].innerHTML) {
+                let combinedTotal = parseInt(squares[i].innerHTML) * parseInt(squares[i+1].innerHTML)
+                squares[i].innerHTML = combinedTotal
+                squares[i+1].innerHTML = 0
+            }
+        }
+    }
+
+    //assign keycodes
+    function control(e) {
+        if(e.keyCode === 39) {
+            keyRight()
+        } else if (e.keyCode === 37) {
+            keyLeft()
+        }
+    }
+    document.addEventListener('keyup', control)
+
+    function keyRight() {
+        moveRight()
+        combineRow()
+        moveLeft()
+        generate()
+    }
+
+    function keyLeft() {
+        moveLeft()
+        combineRow()
+        moveLeft()
+        generate()
+    }
+
+    function keyDown() {
+        moveDown()
+        combineColumn()
+        moveDown()
+        generate()
+    }
+
+    function keyUp() {
+        moveUp()
+        combineColumn()
+        moveUp()
+        generate()
+    }
 
 
 
